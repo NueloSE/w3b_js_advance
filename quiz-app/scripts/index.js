6b045bb24b7223fef3352b8124d956c032da432a
Nuelo <akalo.emmanuel18@gmail.com>
2024-08-28 03:27:07 +0100

    feat: implement clicks and autocheck correct and wrong answer

--- quiz-app/scripts/index.js
+++ quiz-app/scripts/index.js
@@ -7,6 +7,13 @@ const questionCardEl = document.querySelector(".question-card")
 const nextBtn = document.querySelector(".next-quiz-btn")
 const quizCounter = document.querySelector(".quiz-counter")
 
+const quizQuestionEl = document.querySelector(".quiz-question")
+const quizOptionsEl = document.querySelector(".quiz-options")
+
+let tickEl = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossEl = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+
 
 startBtnEl.onclick = ()=>{
 	ruleCardEl.classList.add("active-page")
@@ -41,10 +48,35 @@ function quizCounting(num){
 	quizCounter.innerHTML = '<span><p>'+ num +'</p>of<p>'+ questions.length +'</p>Questions</span>'
 }
 
-function showQuestion(index) {
-	let quizQuestionEl = document.querySelector(".quiz-question")
-	let quizOptionsEl = document.querySelector(".quiz-options")
+function optionSelected(anwser) {
+	let userAns = anwser.textContent;
+	let correctAns = questions[quizCount].answer
+	let allOptions = quizOptionsEl.children.length;
+	
+	if (correctAns == userAns){
+		anwser.classList.add("correct")
+		console.log('correct answer')
+		anwser.insertAdjacentHTML("beforeend", tickEl)
+	} else {
+		anwser.classList.add("incorrect")
+		console.log('incorrect answer')
+		anwser.insertAdjacentHTML("beforeend", crossEl)
 
+		//automatic select correct answer if wrong answer was selected
+		for (let i = 0; i < allOptions; i++) {
+			if (quizOptionsEl.children[i].textContent == correctAns){
+				quizOptionsEl.children[i].setAttribute("class", "option correct")
+				quizOptionsEl.children[i].insertAdjacentHTML("beforeend", tickEl)
+			}			
+		}
+	}
+
+	for (let i = 0; i < allOptions; i++) {
+		quizOptionsEl.children[i].classList.add("disabled")
+	}
+}
+
+function showQuestion(index) {
 	let qustn = "<span>"+ questions[index].number+'.'+ questions[index].question +"</span>"
 	let optionVal = '<div class="option">'+ questions[index].options[0] +'</div>'
 					+ '<div class="option">'+ questions[index].options[1] +'</div>'
@@ -53,11 +85,9 @@ function showQuestion(index) {
 
 	quizQuestionEl.innerHTML = qustn 
 	quizOptionsEl.innerHTML = optionVal
-
+	const optionEl = quizOptionsEl.querySelectorAll(".option")
+	for (let i = 0; i < optionEl.length; i++){
+		optionEl[i].setAttribute("onclick", "optionSelected(this)")
+	}
 	
 }
-
-
-
-
-
